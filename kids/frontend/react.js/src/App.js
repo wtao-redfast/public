676170cf35b0c3957e7b29b894ed67fa5a9f545a
Wenwei Tao <wenweitao@Wenweis-MBP.hsd1.ca.comcast.net>
2020-05-23 18:16:44 -0700

    Add ui widget comment

--- kids/frontend/react.js/src/App.js
+++ kids/frontend/react.js/src/App.js
@@ -4,6 +4,7 @@ import { WeatherContext } from "./viewModel/WeatherViewModel";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 
+// more UI widgets demo from https://v3.material-ui.com/
 function App() {
     const weathers = useContext(WeatherContext);
     return (
