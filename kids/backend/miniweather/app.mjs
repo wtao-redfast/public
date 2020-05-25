150e3f3ff9cfa6fb7b968907f2535e275bf32961
Wenwei Tao <wenweitao@Wenweis-MBP.hsd1.ca.comcast.net>
2020-05-25 14:53:07 -0700

    Done server code

--- kids/backend/miniweather/app.mjs
+++ kids/backend/miniweather/app.mjs
@@ -1,6 +1,46 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
-
-app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(3000);
+
+app.get("/api/location/search", (req, res) => {
+    let woeid = 20200525;
+    switch (req.query.query) {
+        case "shanghai":
+            woeid = 20201212;
+            break;
+        case "san jose":
+            woeid = 20201213;
+            break;
+        default:
+            break;
+    }
+    res.json([
+        {
+            title: req.query.query,
+            woeid: woeid,
+        },
+    ]);
+});
+
+app.get("/api/location/:woeid/", (req, res) => {
+    let the_temp = 0;
+    switch (req.params.woeid) {
+        case "2020121":
+            the_temp = -15;
+            break;
+        case "20201213":
+            the_temp = 17;
+            break;
+        default:
+            break;
+    }
+    res.json({
+        consolidated_weather: [
+            {
+                weather_state_abbr: "lc",
+                the_temp: the_temp,
+            },
+        ],
+    });
+});
