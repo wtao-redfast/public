5bc4df378e306c6e62ee20065ad0a53d42186e26
Wenwei Tao <wenweitao@Wenweis-MBP.hsd1.ca.comcast.net>
2020-05-24 23:10:02 -0700

    Try displaying version

--- kids/frontend/react.js/src/App.js
+++ kids/frontend/react.js/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import { WeatherContext } from "./viewModel/WeatherViewModel";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import {version} from '../package.json';
 
 // more UI widgets demo from https://v3.material-ui.com/
 function App() {
     const weathers = useContext(WeatherContext);
     return (
         <div className="App">
+            <p>version {version}</p>
             <header className="App-header">
                 {weathers.map((weather, index) => (
                     <Paper
