31937ebc252bf8cba32d16ac5323310155acb7c4
wtao-nexus5 <wtao.nexus5@gmail.com>
2020-05-26 02:22:53 +0000

    Updates

---
+++ precache-manifest.517ce2561bf76701fb1879dafd5745f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a91354696a124036d1af1c279bd67f97",
+    "url": "/public/index.html"
+  },
+  {
+    "revision": "862ab47145b06f433910",
+    "url": "/public/static/css/main.7b071f5e.chunk.css"
+  },
+  {
+    "revision": "47968d4ac6c051a489ae",
+    "url": "/public/static/js/2.d8c228d3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/public/static/js/2.d8c228d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "862ab47145b06f433910",
+    "url": "/public/static/js/main.f9fc7a05.chunk.js"
+  },
+  {
+    "revision": "ca78d8bce2e3358f6b46",
+    "url": "/public/static/js/runtime-main.c8a1a6b4.js"
+  }
+]);
