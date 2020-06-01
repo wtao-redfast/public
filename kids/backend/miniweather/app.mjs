be6a79199a52e6cfd7c44a9a3897211b113ff718
Wenwei Tao <wenweitao@Wenweis-MBP.hsd1.ca.comcast.net>
2020-05-31 18:15:25 -0700

    Add unit test

--- kids/backend/miniweather/app.mjs
+++ kids/backend/miniweather/app.mjs
@@ -1,11 +1,11 @@
 import express from "express";
 
-const app = express();
+export const app = express();
 const PORT = process.env.PORT || 3000; //app must listen $PORT in heroku. https://help.heroku.com/P1AVPANS/why-is-my-node-js-app-crashing-with-an-r10-error
-app.listen(PORT);
+export const server = app.listen(PORT);
 
 app.get("/api/location/search", (req, res) => {
-    console.log(req);
+    //console.log(req);
     let woeid = 20200525;
     if (req.query.query == null) {
         console.error("invalid query " + JSON.stringify(req.query));
