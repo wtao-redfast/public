ce2f1294746e015bf2aa60a6105faee86cb0e9ad
Wenwei Tao <wenweitao@Wenweis-MBP.hsd1.ca.comcast.net>
2020-05-31 18:30:33 -0700

    Add file

--- kids/backend/miniweather/__test__/app.test.js
+++ kids/backend/miniweather/__test__/app.test.js
@@ -1,9 +1,17 @@
 //https://itnext.io/writing-tests-in-your-node-js-api-application-cfb5592df466
 
 import supertest from "supertest";
-import { app } from "../app.mjs";
+import { app, server } from "../app.mjs";
 
-it("tests search API", async () => {
-    const response = await supertest(app).get('/api/location/search/?query=shanghai');
-    expect(response.status).toBe(200);
+describe("Test the APIs", () => {
+    afterAll(async () => {
+        await server.close();
+    });
+
+    it("tests search API", async () => {
+        const response = await supertest(app).get(
+            "/api/location/search/?query=shanghai"
+        );
+        expect(response.status).toBe(200);
+    });
 });
